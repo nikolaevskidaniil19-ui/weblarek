66f3d88352093289cb0c55b0bce2de5114464af4
Daniil <daniil.nikolaevski@yandex.by>
2026-07-26 20:17:11 +0300

    финальные правки

--- src/components/models/Catalog.ts
+++ src/components/models/Catalog.ts
@@ -4,37 +4,30 @@ export class Catalog {
   private items: IProduct[] = [];
   private selectedItem: IProduct | null = null;
 
-
-  // Метод для сохранения товаров
-  setItems(items: IProduct[]): void {
-    this.items = items;
-  }
-
-  // Метод для получения всех товаров (возвращает копию)
+ // Метод для получения всех товаров (возвращает копию)
   getItems(): IProduct[] {
     return [...this.items];
   }
-
   // Метод для получения общего количества товаров
   getTotal(): number {
     return this.items.length;
   }
-
   // Метод для получения товара по id
   getItemById(id: IProduct['id']): IProduct | null {
     return this.items.find(item => item.id === id) ?? null;
   }
-
-  // Метод для выбора товара
-  setSelectedItem(item: IProduct | null): void {
-    this.selectedItem = item;
-  }
-
   // Метод для выбора товара напрямую по его id
   setSelectedItemById(id: IProduct['id']): void {
     this.selectedItem = this.getItemById(id);
   }
-
+  // Метод для сохранения товаров
+  setItems(items: IProduct[]): void {
+    this.items = items;
+  }
+  // Метод для выбора товара
+  setSelectedItem(item: IProduct | null): void {
+    this.selectedItem = item;
+  }
   // Метод для получения выбранного товара
   getSelectedItem(): IProduct | null {
     return this.selectedItem;
